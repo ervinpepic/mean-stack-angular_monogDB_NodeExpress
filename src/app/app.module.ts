21b04fd793535381d3e4886aaa54edb91ea394eb
Ervin Pepic <ervin.hack@gmail.com>
2020-01-29 22:11:59 +0100

    Changed the way app works. Make setup for routers and added some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule,
-         MatCardModule,
-         MatButtonModule,
-         MatToolbarModule,
-         MatExpansionModule} from '@angular/material';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -22,6 +25,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     PostListComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
